49783b21eb7b1c0627ba0a43e53811b62f8dc2c0
xixka <xixka@users.noreply.github.com>
2026-08-07 10:45:44 +0800

    fix: vite.config.ts 去掉 async 避免类型不匹配

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import path from "path";
 
 const host = process.env.TAURI_DEV_HOST;
 
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Vite options tailored for Tauri development
   clearScreen: false,
   server: {
     port: 1420,
@@ -34,4 +33,4 @@ export default defineConfig(async () => ({
     minify: "esbuild",
     sourcemap: false,
   },
-}));
+});
